370fcf5c50082715e5c6f149ae9c3a8e1238087a
Arcoz0308 <legrandroi33@gmail.com>
2023-06-08 19:36:33 +0200

    update prisma

---
+++ prisma/migrations/20230608145118_update_2/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `guild.name` on the `GuildMember` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "GuildMember" DROP COLUMN "guild.name";
