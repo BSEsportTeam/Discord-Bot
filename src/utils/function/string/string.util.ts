59c88ef5562fb67892d335deb424e2c5d1637132
Arcoz0308 <legrandroi33@gmail.com>
2023-06-10 18:31:09 +0200

    simplify regex

--- src/utils/function/string/string.util.ts
+++ src/utils/function/string/string.util.ts
@@ -1,6 +1,6 @@
 // code from https://github.com/Virtual-Royaume/Royaume-Discord-Bot/blob/main/src/utils/message/message.util.ts
 export const msgParams = (message: string, params: (string | number)[]): string => {
-	const words = message.match(/\{[^}]+\}/g);
+	const words = message.match(/{[^}]+}/g);
 
 	if (words) for (let i = 0; i < params.length; i++) {
 		message = message.replace(words[i], String(params[i]));
