b650ac1fb9f5f0a54dd9c696f8099abb88ade893
Arcoz0308 <legrandroi33@gmail.com>
2023-06-24 22:39:15 +0200

    feat: add bump checking in message create event
    Add a new `checkBump` function in the `MessageCreate` class to check for bumps in guilds. If the message is from the bump bot and is in the bump channel, it fetches the interaction and checks if it is the bump command. If it is, it creates a new bump in the database and sends a reply with the bump count and XP earned. It also adds XP to the user if the bump count is less than the max bumps set in the global config.

--- src/events/message_create/message_create.class.ts
+++ src/events/message_create/message_create.class.ts
@@ -1,6 +1,7 @@
 import {Event} from '$core/handlers/events';
 import {Dev} from '$core/utils/dev';
 import type {Message} from 'discord.js';
+import {userMention} from 'discord.js';
 import {cooldownCollection} from './message_create.util';
 import {getDevGuildWithId, getGuildWithId} from '$core/config/guilds';
 import {
@@ -16,14 +17,21 @@ import {
 import {logger} from '$core/utils/logger';
 import {isDebuggableError} from '$core/utils/error';
 import {isDev} from '$core/config/env';
+import {globalConfig} from '$core/config/global';
+import {resultify} from 'rustic-error';
+import {createBump, getBumpsByUserInGuildToday} from '$core/handlers/database/bump/bump.func';
+import {simpleEmbed} from '$core/utils/discord';
+import {msgParams} from '$core/utils/function/string';
+import {messageCreateConfig} from '$core/config/message/event/message_create.config';
 
 @Dev
 export default class MessageCreate extends Event<'messageCreate'> {
 	name = 'messageCreate' as const;
 
-	async run(message: Message): Promise<void>  {
+	async run(message: Message): Promise<void> {
 		//check xp
 		void this.checkXP(message);
+		void this.checkBump(message);
 	}
 
 	async checkXP(message: Message) {
@@ -73,6 +81,73 @@ export default class MessageCreate extends Event<'messageCreate'> {
 			return;
 		}
 
-		cooldownCollection.set(`${message.guildId}.${message.author.id}`, message.createdTimestamp + (COOLDOWN_IN_SECONDS*1000));
+		cooldownCollection.set(`${message.guildId}.${message.author.id}`, message.createdTimestamp + (COOLDOWN_IN_SECONDS * 1000));
+	}
+
+	async checkBump(message: Message) {
+		if (!message.guildId || !message.guild) {
+			return;
+		}
+		const guildId = message.guildId;
+
+		if (!message.author.bot) {
+			return;
+		}
+
+		const botId = isDev ? '742097999198683176' : globalConfig.bumpBot;
+		if (message.author.id !== botId) {
+			return;
+		}
+
+		const bumpChannel = (isDev ? getDevGuildWithId(guildId) : getGuildWithId(guildId))?.bumpChannel || '';
+		if (message.channelId !== bumpChannel) {
+			return;
+		}
+
+		const messageResult = await resultify(() => message.fetch());
+		if (!messageResult.ok) {
+			logger.error('failed to fetch message for bump check');
+			return;
+		}
+		if (!messageResult.value.interaction || messageResult.value.interaction.commandName !== globalConfig.bumpCommand) {
+			return;
+		}
+
+		const authorId = messageResult.value.interaction.user.id;
+		const bumpResult = await getBumpsByUserInGuildToday(authorId, guildId);
+		if (!bumpResult.ok) {
+			logger.error(`failed to fetch bumps of user, error : ${bumpResult.error.message}`, bumpResult.error.debug());
+			return;
+		}
+
+		const create = async () => {
+			const bumpCreate = await createBump({
+				userId: authorId,
+				guildId: guildId
+			});
+			if (!bumpCreate.ok) {
+				logger.error(`failed to create bump to database, error : ${bumpCreate.error.message}`, bumpCreate.error.debug());
+			}
+		};
+		void create();
+
+		const replyResult = await resultify(() => message.reply({
+			embeds: [
+				simpleEmbed(msgParams(messageCreateConfig.bump.description, [
+					userMention(authorId),
+					bumpResult.value.length + 1,
+					globalConfig.maxBump,
+					globalConfig.maxBump > bumpResult.value.length ? globalConfig.xpPerBump : 0
+				]), messageCreateConfig.bump.title)
+			]
+		}));
+		if (!replyResult.ok) {
+			logger.error(`failed to send thx message for bump, error : ${replyResult.error.message}`);
+		}
+
+		if (globalConfig.maxBump <= bumpResult.value.length) {
+			return;
+		}
+		void addXp(authorId, guildId, globalConfig.xpPerBump);
 	}
 }
