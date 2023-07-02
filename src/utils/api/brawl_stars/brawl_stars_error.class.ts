2b6b8e5590abc158f129557be583b113aa130374
Arcoz0308 <legrandroi33@gmail.com>
2023-07-02 13:56:21 +0200

    feat(api): add BrawlStarsApiError class
    Adds the BrawlStarsApiError class to handle errors in the Brawl Stars API. It extends the RestRequestError class and includes additional properties for the reason and API message.

---
+++ src/utils/api/brawl_stars/brawl_stars_error.class.ts
@@ -0,0 +1,17 @@
+import type {DebugValues} from '$core/utils/logger';
+import {RestRequestError} from '$core/utils/error';
+
+
+export class BrawlStarsApiError extends RestRequestError {
+	constructor(message: string, baseError: RestRequestError, public reason: string, public apiMessage: string) {
+		super(message, baseError.code, baseError.url, baseError.method, baseError.response);
+	}
+
+	debug(): DebugValues {
+		return {
+			...super.debug(),
+			'bs api reason': this.reason,
+			'bs api message': this.apiMessage
+		};
+	}
+}
