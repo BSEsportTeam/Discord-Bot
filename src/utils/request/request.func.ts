a19cf013ad3a407f3a6b46462c38f7585160a8df
Arcoz0308 <legrandroi33@gmail.com>
2023-07-02 13:38:44 +0200

    feat(utils/request): implement restRequest and jsonRestRequest functions
    This commit adds two new functions to the `request.func.ts` file in the `src/utils/request` directory. The `restRequest` function is responsible for making a REST request using the provided method, URL, and options. If the response is not successful, it returns a `RestRequestError` object. Otherwise, it returns the response.
    
    The `jsonRestRequest` function is similar to `restRequest`, but it sets the `Content-Type` header to `application/json` before making the request. If the result of `restRequest` is not successful, it returns the error. Otherwise, it returns the response parsed as JSON.
    
    This commit addresses the need for making REST requests in the application.

---
+++ src/utils/request/request.func.ts
@@ -0,0 +1,25 @@
+import type {Method, RequestParams} from '$core/utils/request/request.type';
+import {RestRequestError} from '$core/utils/error';
+import type {Result} from 'rustic-error';
+import {error, ok} from 'rustic-error';
+
+export const restRequest = async (method: Method, url: string, options: RequestParams = {}): Promise<Result<Response, RestRequestError>> => {
+	const response = await fetch(url, {...options, method: method});
+
+	if (!response.ok) {
+		return error(new RestRequestError('failed to rest request', response.status, url, method, response));
+	}
+
+	return ok(response);
+};
+
+export const jsonRestRequest = async (method: Method, url: string, options: RequestParams = {}): Promise<Result<object, RestRequestError>> => {
+	options.headers = {...options.headers, 'Content-Type': 'application/json'};
+
+	const result = await restRequest(method, url, options);
+	if (!result.ok) {
+		return error(result.error);
+	}
+
+	return ok(await result.value.json());
+};
