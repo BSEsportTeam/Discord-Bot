d451388a0bd3de1ac3e60fbdc53bd5bf1ad389a7
Arcoz0308 <legrandroi33@gmail.com>
2023-07-02 12:30:21 +0200

    feat(utils): remove unused code in error.type.ts

--- src/utils/error/error.type.ts
+++ src/utils/error/error.type.ts
@@ -3,21 +3,3 @@ import type {DebugValues} from '$core/utils/logger/';
 export interface DebuggableError extends Error{
 	debug(): DebugValues;
 }
-
-export interface InteractionDebugsBase extends DebugValues {
-	user: string;
-	channel: string
-	guild: string
-}
-
-export interface CommandDebugsBase extends InteractionDebugsBase {
-	'command options': string;
-}
-
-export type CommandDebugs = CommandDebugsBase|Omit<CommandDebugsBase, 'channel'|'guild'>
-
-export interface ButtonDebugsBase extends InteractionDebugsBase {
-	customId: string;
-}
-
-export type ButtonDebugs = ButtonDebugsBase|Omit<ButtonDebugsBase, 'channel'|'guild'>;
