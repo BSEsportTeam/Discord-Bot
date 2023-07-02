f2ac4d77e5e312f93cd97232309040aec084046a
Arcoz0308 <legrandroi33@gmail.com>
2023-07-02 12:34:13 +0200

    feat(utils/error): add RestRequestError class
    - Added a new file `src/utils/error/rest_request_error.class.ts` which
      defines the `RestRequestError` class.
    - The `RestRequestError` class extends the `Error` class and implements
      the `DebuggableError` interface.
    - The class has a constructor that accepts a `message` string, `code`
      number, `url` string, and `method` string as parameters.
    - The `debug` method returns an object with the debug values of the error,
      including the `method`, `code`, and `url`.

---
+++ src/utils/error/rest_request_error.class.ts
@@ -0,0 +1,17 @@
+import type {DebuggableError} from '$core/utils/error/error.type';
+import type {DebugValues} from '$core/utils/logger';
+import type {Method} from '$core/utils/request/request.type';
+
+export class RestRequestError extends Error implements DebuggableError {
+	constructor(message: string, public code: number, public url: string, public method: Method) {
+		super(message);
+	}
+
+	debug(): DebugValues {
+		return {
+			method: `${this.method}`,
+			code: `${this.code}`,
+			url: this.url
+		};
+	}
+}
