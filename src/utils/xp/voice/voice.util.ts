da14ba5d5c26fb1bab2af02df43241dd9fe4460f
Arcoz0308 <legrandroi33@gmail.com>
2023-07-04 23:27:00 +0200

    feat(voice.type.ts, voice.util.ts): add voice related types and utility functions

---
+++ src/utils/xp/voice/voice.util.ts
@@ -0,0 +1,14 @@
+import type {VoiceCollection, VoiceCollectionKey, VoiceInfos} from '$core/utils/xp/voice/voice.type';
+import {Collection} from 'discord.js';
+import type {Snowflake} from 'discord-api-types/globals';
+
+const voicesInfos: VoiceCollection = new Collection();
+
+const serialize = (guildId: Snowflake, userId: Snowflake): VoiceCollectionKey => `${guildId}-${userId}`;
+
+export const setVoice = (info: VoiceInfos) => voicesInfos.set(serialize(info.guildId, info.id), info);
+
+export const getVoices = () => voicesInfos.values();
+
+export const getVoice = (guildId: Snowflake, userId: Snowflake) => voicesInfos.get(serialize(guildId, userId));
+export const removeVoice = (guildId: Snowflake, userId: Snowflake) => voicesInfos.delete(serialize(guildId, userId));
