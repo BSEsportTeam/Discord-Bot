673e75c2c498dd5abb62a8b42850e7796497b91b
Arcoz0308 <legrandroi33@gmail.com>
2023-07-04 23:21:20 +0200

    feat(xp.const.ts): add XP_PER_MINUTE_UNMUTE and XP_PER_MINUTE_MUTE constants to calculate XP gain per minute during unmute and mute actions

--- src/utils/xp/xp.const.ts
+++ src/utils/xp/xp.const.ts
@@ -5,3 +5,5 @@ export const LONG_MESSAGE_MIN_LENGTH = 500;
 export const COOLDOWN_IN_SECONDS = 20;
 export const BOOSTER_MULTIPLIER = 2;
 export const XP_BOOST_MULTIPLIER = 3;
+export const XP_PER_MINUTE_UNMUTE = 10;
+export const XP_PER_MINUTE_MUTE = 5;
