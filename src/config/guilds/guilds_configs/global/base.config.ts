8e701fa472edf96206205b788f5c66982b583af9
Arcoz0308 <legrandroi33@gmail.com>
2023-07-08 17:07:05 +0200

    add config for server global

---
+++ src/config/guilds/guilds_configs/global/base.config.ts
@@ -0,0 +1,17 @@
+import type {BaseGuild} from '../../guild.type';
+import {xpConfig} from './xp.config';
+import {pubMessage} from './pub_message.config';
+
+export const globalGuildConfig: BaseGuild = {
+	name: 'global',
+	guildId: '346989473143455744',
+	eventAnnouncements: {
+		channelId: '782687067674771467',
+		roleId: '849734730596483092',
+		enable: false
+	},
+	inviteLink: 'https://discord.gg/F2GTeYT',
+	xp: xpConfig,
+	bumpChannel: '979400972315000852',
+	pubMessages: pubMessage,
+};
