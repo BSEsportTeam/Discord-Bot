d675a6f931c0582ed97fcaf1acfed6fe115b90bf
Arcoz0308 <legrandroi33@gmail.com>
2023-07-08 17:06:31 +0200

    add config for server clash royale

---
+++ src/config/guilds/guilds_configs/clash_royale/base.config.ts
@@ -0,0 +1,17 @@
+import type {BaseGuild} from '../../guild.type';
+import {xpConfig} from './xp.config';
+import {pubMessage} from './pub_message.config';
+
+export const clashRoyaleGuildConfig: BaseGuild = {
+	name: 'clashRoyale',
+	guildId: '795972160023822346',
+	eventAnnouncements: {
+		channelId: '1094658713215836281',
+		roleId: '828351231243714591',
+		enable: true
+	},
+	inviteLink: 'https://discord.gg/yvKtPw7GCr',
+	xp: xpConfig,
+	bumpChannel: '1006679096077602977',
+	pubMessages: pubMessage
+};
