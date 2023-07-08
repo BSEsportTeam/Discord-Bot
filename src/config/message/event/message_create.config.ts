4b1afa7be90d896465e821ae77853bb52c462bab
Arcoz0308 <legrandroi33@gmail.com>
2023-07-08 09:41:47 +0200

    fix(message_create.config.ts) first corrections

--- src/config/message/event/message_create.config.ts
+++ src/config/message/event/message_create.config.ts
@@ -2,9 +2,9 @@ import type {BaseMessage} from '$core/config/message';
 
 export const messageCreateConfig = {
 	bump: {
-		title: 'Merci du bump ! :tada:',
-		description: 'Merci à {mention} d\'avoir bump le serveur ! Hésitez pas à revenir dans 2h pour bumper le serveur à nouveau ! \n\n' +
-			'bumps journaliers : **{bumps}/{bumpsMax}**\n' +
-			'récompense : **{xp} xp !**'
+		title: 'Merci du bump ! 🎉',
+		description: 'Merci à {mention} d\'avoir bump le serveur ! N\'hésitez pas à revenir dans 2h pour bumper le serveur à nouveau !\n\n' +
+			'Bumps journaliers : **{bumps}/{bumpsMax}**\n' +
+			'Récompense : **{xp} XP !**'
 	}
 } satisfies Record<string, BaseMessage>;
