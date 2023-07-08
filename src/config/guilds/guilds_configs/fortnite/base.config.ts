1706778999efd7bbdb8b898e4f7798b0b97e97f9
Arcoz0308 <legrandroi33@gmail.com>
2023-07-08 17:06:44 +0200

    add config for server fortnite

---
+++ src/config/guilds/guilds_configs/fortnite/base.config.ts
@@ -0,0 +1,17 @@
+import type {BaseGuild} from '../../guild.type';
+import {xpConfig} from './xp.config';
+import {pubMessage} from './pub_message.config';
+
+export const fortniteGuildConfig: BaseGuild = {
+	name: 'fortnite',
+	guildId: '805892482336423976',
+	eventAnnouncements: {
+		channelId: '913214792423055410',
+		roleId: '886738721381158962',
+		enable: true
+	},
+	inviteLink: 'https://discord.gg/UHtb89ZkHh',
+	xp: xpConfig,
+	bumpChannel: '1006681147318747226',
+	pubMessages: pubMessage,
+};
