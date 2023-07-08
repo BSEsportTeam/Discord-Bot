845ad7e0303c822b2d33736cf1ec6380ee48db41
Arcoz0308 <legrandroi33@gmail.com>
2023-07-08 17:42:35 +0200

    feat(add_guilds_to_db.ts): add script to add guilds to the database

---
+++ script/add_guilds_to_db/add_guilds_to_db.ts
@@ -0,0 +1,19 @@
+import {prisma} from '$core/handlers/database/prisma';
+import type {Guild} from '@prisma/client';
+import {guildsConfig} from '$core/config/guilds';
+
+const data: Pick<Guild, 'name' | 'id'>[] = [];
+for (const guild of Object.values(guildsConfig)) {
+	data.push({
+		name: guild.name,
+		id: guild.guildId
+	});
+}
+
+prisma.guild.createMany({
+	data: data
+})
+	.then(() => console.log(`add ${data.length} guilds in db`))
+	.catch((e) => {
+		throw e;
+	});
