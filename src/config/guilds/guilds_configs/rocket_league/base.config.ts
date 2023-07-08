650980991c2c1a81982624b0745b01ca3f609d1c
Arcoz0308 <legrandroi33@gmail.com>
2023-07-08 17:07:26 +0200

    add config for server rocket league

---
+++ src/config/guilds/guilds_configs/rocket_league/base.config.ts
@@ -0,0 +1,17 @@
+import type {BaseGuild} from '../../guild.type';
+import {xpConfig} from './xp.config';
+import {pubMessage} from './pub_message.config';
+
+export const rocketLeagueGuildConfig: BaseGuild = {
+	name: 'rocketLeague',
+	guildId: '783421034455105556',
+	eventAnnouncements: {
+		channelId: '834540080995237888',
+		roleId: '815254897200463872',
+		enable: true
+	},
+	inviteLink: 'https://discord.gg/ADPKAzt5hz',
+	xp: xpConfig,
+	bumpChannel: '1006680512041058405',
+	pubMessages: pubMessage,
+};
