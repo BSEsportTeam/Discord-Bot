c4a8094eef7a9d2870b55731c72497460951e9a2
Arcoz <62679207+Arcoz0308@users.noreply.github.com>
2023-07-18 22:42:33 +0200

    Update command_run.ts

--- src/handlers/commands/command_run/command_run.ts
+++ src/handlers/commands/command_run/command_run.ts
@@ -30,13 +30,15 @@ export const commandRun = async (interaction: ChatInputCommandInteraction) => {
 	}
 
 	const result = await command.run(interaction);
-
+	
+	const commandName = interaction.commandName +
+		(interaction.options.getSubcommandGroup() !== null ? '.' + interaction.options.getSubcommandGroup() : '') +
+		(interaction.options.getSubcommand(false) !== null ? '.' + interaction.options.getSubcommand(false) : '');
 	if (!result.ok) {
-		const commandName = interaction.commandName +
-			(interaction.options.getSubcommandGroup() !== null ? '.' + interaction.options.getSubcommandGroup() : '' ) +
-			(interaction.options.getSubcommand(false) !== null ? '.' + interaction.options.getSubcommand(false) : '');
-		logger.error(`Error with command ${effect.bold+commandName.toLocaleUpperCase()+effectReset.bold} : ${forground256Color(202)}${result.error.message}`, result.error.debug());
+		logger.error(`Error with command ${effect.bold + commandName.toLocaleUpperCase() + effectReset.bold} : ${forground256Color(202)}${result.error.message}`, result.error.debug());
 
 		await replyError(interaction, command.preReply.ephemeral, command.preReply.enable);
+	} else {
+		logger.info(`COMMANDS RUN: ${interaction.user.username} used command ${commandName}, result : ${result.value ? 'success' : 'failed'}`);
 	}
 };
