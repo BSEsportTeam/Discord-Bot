baef977b12c81dfcb8389097f84c9b2bd79f1be6
Arcoz0308 <legrandroi33@gmail.com>
2023-07-21 20:05:47 +0200

    feat(pub_messages.util.ts): refactor getMinMessages function to use DayJS diff method for improved readability and maintainability
    (fix #28)

--- src/tasks/pub_messages/pub_messages.util.ts
+++ src/tasks/pub_messages/pub_messages.util.ts
@@ -3,19 +3,18 @@ import {
 	MIN_MESSAGES_ONE_DAY,
 	MIN_MESSAGES_SAME_DAY,
 	MIN_MESSAGES_THREE_DAYS,
-	MIN_MESSAGES_TWO_DAYS,
-	SECOND_MESSAGE_HOUR
+	MIN_MESSAGES_TWO_DAYS
 } from '$core/tasks/pub_messages/pub_messages.const';
 import type {PubMessageConfig} from '$core/config/guilds';
 
 export const getMinMessages = (lastSend: Date): number => {
-	if (DayJS().add(5, 'minutes').hour() === SECOND_MESSAGE_HOUR) {
+	if (DayJS().diff(DayJS(lastSend), 'day') === 0) {
 		return MIN_MESSAGES_SAME_DAY;
 	}
-	if (DayJS().diff(DayJS(lastSend)) === 1) {
+	if (DayJS().diff(DayJS(lastSend), 'day') === 1) {
 		return MIN_MESSAGES_ONE_DAY;
 	}
-	if (DayJS().diff(DayJS(lastSend)) === 2) {
+	if (DayJS().diff(DayJS(lastSend), 'day') === 2) {
 		return MIN_MESSAGES_TWO_DAYS;
 	}
 
