4e2399c7e1ff6c54bec2b10e51aa99ccbcd628bf
Arcoz0308 <legrandroi33@gmail.com>
2023-07-23 15:01:30 +0200

    feat(random.func.ts): add getRandomValueFromArray function to generate random value from array

--- src/utils/function/random/random.func.ts
+++ src/utils/function/random/random.func.ts
@@ -2,3 +2,12 @@ export const generateRandomValue = (max: number): number => {
 	const random = Math.random();
 	return Math.floor(random * (max + 1));
 };
+
+export const getRandomValueFromArray = <T>(array: T[]): T | undefined => {
+	if (array.length === 0) {
+		return undefined; // Return undefined if the array is empty
+	}
+
+	const randomIndex = Math.floor(Math.random() * array.length);
+	return array[randomIndex];
+};
