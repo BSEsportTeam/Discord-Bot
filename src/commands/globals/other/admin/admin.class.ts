5b896ada963b98ccab862ffa05205f3e42d5fb9e
Arcoz0308 <legrandroi33@gmail.com>
2023-07-23 12:35:21 +0200

    feat(admin.class.ts): add @Dev decorator to the TopLevel class to indicate development mode

--- src/commands/globals/other/admin/admin.class.ts
+++ src/commands/globals/other/admin/admin.class.ts
@@ -3,7 +3,9 @@ import {BaseCommand} from '$core/handlers/commands';
 import {builder} from './admin.builder';
 import {commandsConfig} from '$core/config/message/command';
 import {ReverseXpMovement} from '$core/commands/globals/other/admin/reverse_xp_movement/reverse_xp_movement';
+import {Dev} from '$core/utils/dev';
 
+@Dev
 export default class TopLevel extends BaseCommand {
 	builder = builder.toJSON();
 
