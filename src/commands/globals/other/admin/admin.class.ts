9686f2a633d94072341f17e6b7367e8b729dde72
Arcoz0308 <legrandroi33@gmail.com>
2023-08-01 23:30:00 +0200

    fix(admin.class.ts): fix import path for ReverseXpMovement class
    fix(reverse_xp_movement.class.ts): rename file to match class name

--- src/commands/globals/other/admin/admin.class.ts
+++ src/commands/globals/other/admin/admin.class.ts
@@ -2,7 +2,7 @@ import type {SubCommandOptions} from '$core/handlers/commands';
 import {BaseCommand} from '$core/handlers/commands';
 import {builder} from './admin.builder';
 import {commandsConfig} from '$core/config/message/command';
-import {ReverseXpMovement} from '$core/commands/globals/other/admin/reverse_xp_movement/reverse_xp_movement';
+import {ReverseXpMovement} from '$core/commands/globals/other/admin/reverse_xp_movement/reverse_xp_movement.class';
 import {Dev} from '$core/utils/dev';
 
 @Dev
