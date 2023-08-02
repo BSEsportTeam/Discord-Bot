d2a6c005c860ec34a50258d00a66467c814097bc
Arcoz0308 <legrandroi33@gmail.com>
2023-08-02 19:18:12 +0200

    feat(env.z.ts): add GOOGLE_MAIL, GOOGLE_PRIVATE_KEY, and GOOGLE_SHEETS_ID environment variables to the envDTO object

--- src/config/env/env.z.ts
+++ src/config/env/env.z.ts
@@ -2,5 +2,8 @@ import {z} from 'zod';
 
 export const envDTO = z.object({
 	TOKEN: z.string().nonempty(),
-	BRAWL_STARS_TOKEN: z.string().nonempty()
+	BRAWL_STARS_TOKEN: z.string().nonempty(),
+	GOOGLE_MAIL: z.string().email().nonempty(),
+	GOOGLE_PRIVATE_KEY: z.string().nonempty(),
+	GOOGLE_SHEETS_ID: z.string().nonempty()
 });
