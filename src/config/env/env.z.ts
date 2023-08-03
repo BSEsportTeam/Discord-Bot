35c69124ce9d60c66a14544115a1f68f89861934
Arcoz0308 <legrandroi33@gmail.com>
2023-08-03 12:17:42 +0200

    feat(env.z.ts): add support for WEBHOOK_DISCORD_URL environment variable to be able to use a Discord webhook for notifications

--- src/config/env/env.z.ts
+++ src/config/env/env.z.ts
@@ -2,5 +2,6 @@ import {z} from 'zod';
 
 export const envDTO = z.object({
 	TOKEN: z.string().nonempty(),
-	BRAWL_STARS_TOKEN: z.string().nonempty()
+	BRAWL_STARS_TOKEN: z.string().nonempty(),
+	WEBHOOK_DISCORD_URL: z.string().nonempty()
 });
