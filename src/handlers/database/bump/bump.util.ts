e1b1f8ee0708b1d674e16ba8b6177a8d508676ca
Arcoz0308 <legrandroi33@gmail.com>
2023-08-03 23:10:22 +0200

    quick fix

--- src/handlers/database/bump/bump.util.ts
+++ src/handlers/database/bump/bump.util.ts
@@ -1,5 +1,5 @@
 import {DayJS} from '$core/utils/function/dayjs/dayjs';
 
 export const getToday = (): Date => {
-	return DayJS().tz().set('hour', 0).set('minute', 0).set('seconds', 0).toDate();
+	return DayJS().tz().set('hour', 0).set('minute', 0).set('seconds', 0).utc().toDate();
 };
