46cb4f138bc210b822986985d80da952d1c8063d
Arcoz0308 <legrandroi33@gmail.com>
2023-09-05 17:51:17 +0200

    feat(brawl_stars_stats.class.ts): add BrawlStarsStats class

---
+++ src/tasks/brawl_stars_stats/brawl_stars_stats.class.ts
@@ -0,0 +1,10 @@
+import {Task, TaskType} from '$core/handlers/task';
+
+export class BrawlStarsStaff extends Task<null> {
+	interval = '0 0 * * *';
+	type = TaskType.CRON_INTERVAL;
+
+	async onTick() {
+
+	}
+}
