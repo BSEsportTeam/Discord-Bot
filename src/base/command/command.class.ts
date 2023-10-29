647b836b5c073d0d051237ce737c119420603208
Arcoz0308 <legrandroi33@gmail.com>
2023-10-29 22:29:12 +0100

    chore(command.class.ts): refactor Command class
    - Removed unnecessary imports and updated import types
    - Changed the abstract property `slashBuilder` to be required for all subclasses
    - Removed unused methods (`preRun`, `isChatInput`, `isProfileInput`, `isMessageInput`)
    - Added abstract method `run` to handle command execution
    - Updated `toJSON` method to return the JSON representation of `slashBuilder`

--- src/base/command/command.class.ts
+++ src/base/command/command.class.ts
@@ -1,15 +1,7 @@
 import type {Client} from '$core/client';
 import type {Service} from '$core/base/service/service.class';
-import type {
-	ChatCommand,
-	CommandInteraction,
-	GuildAlias,
-	MessageCommand,
-	ProfileCommand,
-	SubCommandGroups,
-	SubCommands
-} from '$core/base/command/command.type';
-import type {SlashCommandBuilder} from 'discord.js';
+import type {CommandResult, GuildAlias, SubCommandGroups, SubCommands} from '$core/base/command/command.type';
+import type {ChatInputCommandInteraction, SlashCommandBuilder} from 'discord.js';
 
 export abstract class Command<S extends Service> {
 	readonly client: Client;
@@ -17,8 +9,7 @@ export abstract class Command<S extends Service> {
 
 	guild: GuildAlias = 'all';
 	abstract name: string;
-	slashBuilder?: SlashCommandBuilder;
-	messageBuilder?: MessageComm;
+	abstract slashBuilder: SlashCommandBuilder;
 
 	subCommandGroups: SubCommandGroups<S> = new Map();
 	subCommands: SubCommands<S> = new Map();
@@ -32,19 +23,8 @@ export abstract class Command<S extends Service> {
 		return this.slashBuilder.toJSON();
 	}
 
-	protected async preRun(interaction: CommandInteraction) {
+	abstract run(interaction: ChatInputCommandInteraction): Promise<CommandResult>;
 
-	}
-
-	private isChatInput(): this is ChatCommand<S> {
-		return 'runChat' in this && typeof this.runChat === 'function';
-	}
-
-	private isProfileInput(): this is ProfileCommand<S> {
-		return 'runProfile' in this && typeof this.runProfile === 'function';
-	}
-
-	private isMessageInput(): this is MessageCommand<S> {
-		return 'runMessage' in this && typeof this.runMessage === 'function';
+	protected async preRun(interaction: ChatInputCommandInteraction) {
 	}
 }
