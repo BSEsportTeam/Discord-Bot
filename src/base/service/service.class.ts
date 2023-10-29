c131b688310f05b5e1d14e5a18bacdd18926fe77
Arcoz0308 <legrandroi33@gmail.com>
2023-10-29 22:30:29 +0100

    chore(service.class.ts): refactor reload(), preLoad(), and preUnload() methods

--- src/base/service/service.class.ts
+++ src/base/service/service.class.ts
@@ -2,52 +2,59 @@ import type {Client} from '$core/client';
 import {serviceHasLoad, serviceHasUnload} from '$core/base/service/service.util';
 import {logger} from '$core/utils/logger';
 import {anyToError} from '$core/utils/error';
+import type {CommandList} from '$core/base/service/service.type';
 
 export abstract class Service {
 	client: Client;
 	abstract name: string;
-	abstract version: string;
+	reloadable = true;
+	loadCount = 0;
+	commands: CommandList<typeof this> = new Map();
 
 	protected constructor(client: Client) {
 		this.client = client;
 	}
 
-	async reload(): Promise<boolean> {
+	protected async reload(): Promise<boolean> {
+		if (!this.reloadable) {
+			return false;
+		}
 		await this.preUnload();
 		return this.preLoad();
 	}
 
 	protected async preLoad(): Promise<boolean> {
-		logger.info(`loading ${this.name} (v${this.version})`);
+		logger.info(`loading ${this.name}.`);
 		if (serviceHasLoad(this)) {
 			try {
 				await this.load();
 			} catch (e) {
-				logger.error(`failed to load service ${this.name} (v${this.version})`);
+				logger.error(`failed to load service ${this.name}.`);
 				logger.debugValues({
 					error: anyToError(e).message
 				});
 				return false;
 			}
 		}
-		logger.info(`loaded ${this.name} (v${this.version})`);
+		logger.info(`loaded ${this.name}.`);
+		this.loadCount++;
 		return true;
 	}
 
 	protected async preUnload() {
-		logger.info(`unloading ${this.name} (v${this.version})`);
+		logger.info(`unloading ${this.name}.`);
 		if (serviceHasUnload(this)) {
 			try {
 				await this.unload();
 			} catch (e) {
-				logger.error(`failed to unload service ${this.name} (v${this.version})`);
+				logger.error(`failed to unload service ${this.name}.`);
 				logger.debugValues({
 					error: anyToError(e).message
 				});
 				return;
 			}
 		}
-		logger.info(`unloaded ${this.name} (v${this.version})`);
+		logger.info(`unloaded ${this.name}.`);
 	}
 
 }
