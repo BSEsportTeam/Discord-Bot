a2806f5f75e4efc236a4bbf0c10ab665fb0a2e1b
Arcoz0308 <legrandroi33@gmail.com>
2023-10-29 22:26:31 +0100

    chore(command.type.ts): remove unused imports and interfaces

--- src/base/command/command.type.ts
+++ src/base/command/command.type.ts
@@ -1,11 +1,5 @@
 import type {GuildName} from '$core/config/guilds';
-import type {
-	ChatInputCommandInteraction,
-	MessageContextMenuCommandInteraction,
-	UserContextMenuCommandInteraction
-} from 'discord.js';
 import type {CommandError} from '$core/utils/error';
-import type {Command} from '$core/base/command/command.class';
 import type {Service} from '$core/base/service/service.class';
 import type {SubCommand} from '$core/base/command/sub_command.class';
 
@@ -26,23 +20,6 @@ export type CommandResultOk = [null, CommandResultInfos];
 export type CommandResult = CommandResultErr | CommandResultOk;
 
 export type GuildAlias = 'all' | GuildName;
-
-export type CommandInteraction =
-	ChatInputCommandInteraction
-	| MessageContextMenuCommandInteraction
-	| UserContextMenuCommandInteraction;
-
-export interface ChatCommand<S extends Service> extends Command<S> {
-	runChat(interaction: ChatInputCommandInteraction): Promise<CommandResult>;
-}
-
-export interface MessageCommand<S extends Service> extends Command<S> {
-	runMessage(interaction: MessageContextMenuCommandInteraction): Promise<CommandResult>;
-}
-
-export interface ProfileCommand<S extends Service> extends Command<S> {
-	runProfile(interaction: UserContextMenuCommandInteraction): Promise<CommandResult>;
-}
 
 export type SubCommandGroups<S extends Service> = Map<string, SubCommands<S>>
 export type SubCommands<S extends Service> = Map<string, SubCommand<S>>
