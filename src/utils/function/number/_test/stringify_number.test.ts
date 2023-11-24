12e14f6ce19c04cf60e703a73db1e7c7f3e6cb4f
Arcoz0308 <legrandroi33@gmail.com>
2023-11-24 20:24:11 +0100

    feat(utils/function/number): add tests for stringifyNumber function

---
+++ src/utils/function/number/_test/stringify_number.test.ts
@@ -0,0 +1,29 @@
+import {assert, describe, it} from 'vitest';
+import {stringifyNumber} from '$core/utils/function/number';
+
+describe('stringifyNumber', () => {
+	it('should return 1 000 000', () => {
+		const result = stringifyNumber(1000000);
+		assert.equal(result, '1 000 000');
+	});
+
+	it('should return 100', () => {
+		const result = stringifyNumber(100);
+		assert.equal(result, '100');
+	});
+
+	it('should return 0', () => {
+		const result = stringifyNumber(0);
+		assert.equal(result, '0');
+	});
+
+	it('should return -1 000 000', () => {
+		const result = stringifyNumber(-1000000);
+		assert.equal(result, '-1 000 000');
+	});
+
+	it('should return 100\'000', () => {
+		const result = stringifyNumber(100000, '\'');
+		assert.equal(result, '100\'000');
+	});
+});
