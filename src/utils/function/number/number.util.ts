540b2070e99735ea963c0ee07bd5a6ae65247e83
Arcoz0308 <legrandroi33@gmail.com>
2023-11-24 20:23:18 +0100

    refactor(number.util.ts): refactor stringifyNumber function and add comments

--- src/utils/function/number/number.util.ts
+++ src/utils/function/number/number.util.ts
@@ -8,3 +8,8 @@ export const inverseNumber = (num: number): number => {
 	}
 	return -Math.abs(num);
 };
+
+// change 1000000 to 1 000 000
+export const stringifyNumber = (num: number, separator = ' '): string => {
+	return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator);
+};
