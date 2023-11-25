1ae03810b07e09a33ace9186abe73ccc97d02a15
Arcoz0308 <legrandroi33@gmail.com>
2023-11-25 14:20:50 +0100

    chore(prisma/seeder/text.seeder.ts): add text seeder file

---
+++ prisma/seeder/text.seeder.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async(): Promise<void> => {
+  const texts = {
+    "command.cooldown": "La commande a un cooldown, veillez patienter {seconds} secondes.",
+  };
+
+  const values = await prisma.text.findMany();
+
+  const toAdd: { id: string; value: string }[] = [];
+
+  for (const [id, text] of Object.entries(texts)) {
+    if (!values.find((value) => value.id === id)) {
+      toAdd.push({ id: id, value: text });
+    }
+  }
+
+  if (toAdd.length > 0) {
+    await prisma.text.createMany({
+      data: toAdd,
+    });
+  }
+};
+
+
+main().then(() => {
+  console.log("done");
+  void prisma.$disconnect();
+}).catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
