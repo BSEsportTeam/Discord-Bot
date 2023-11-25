bcf15604c4fc15d579e82cc39097bc1029c5403e
Arcoz0308 <legrandroi33@gmail.com>
2023-11-25 14:00:30 +0100

    chore(command.type.ts): update FailsReasons enum

--- src/base/command/command.type.ts
+++ src/base/command/command.type.ts
@@ -1,7 +1,7 @@
-import type {GuildName} from '$core/config/guilds';
-import type {CommandError} from '$core/utils/error';
-import type {Service} from '$core/base/service/service.class';
-import type {SubCommand} from '$core/base/command/sub_command.class';
+import type { GuildName } from "$core/config/guilds";
+import type { CommandError } from "$core/utils/error";
+import type { Service } from "$core/base/service/service.class";
+import type { SubCommand } from "$core/base/command/sub_command.class";
 
 export type CommandResultInfosPassed = {
 	pass: true;
@@ -13,13 +13,19 @@ export type CommandResultInfoFailed = {
 }
 export type CommandResultInfos = CommandResultInfosPassed | CommandResultInfoFailed;
 
-export type FailsReasons = 'NO_PERMS' | 'VALUE_NOT_FOUND';
+export type FailsReasons =
+	"NO_PERMS"
+	| "VALUE_NOT_FOUND"
+	| "VALUE_NOT_VALID"
+	| "COOLDOWN"
+	| "UNKNOWN"
+	| "NOT_IMPLEMENTED";
 
 export type CommandResultErr = [false, CommandError];
 export type CommandResultOk = [true, CommandResultInfos];
 export type CommandResult = CommandResultErr | CommandResultOk;
 
-export type GuildAlias = 'all' | GuildName;
+export type GuildAlias = "all" | GuildName;
 
 export type SubCommandGroups<S extends Service> = Map<string, SubCommands<S>>
 export type SubCommands<S extends Service> = Map<string, SubCommand<S>>
