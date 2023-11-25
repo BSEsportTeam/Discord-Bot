be200dee9daf56bd7c4dfd3eefe350a4d5a693cf
Arcoz0308 <legrandroi33@gmail.com>
2023-11-25 17:46:21 +0100

    feat(database/text/text.manager.ts): add TextManager class
    This commit adds the TextManager class to the src/manager/database/text/text.manager.ts file. The TextManager class is responsible for managing text data in the database. It includes methods for loading, retrieving, and reloading text data.

---
+++ src/manager/database/text/text.manager.ts
@@ -0,0 +1,43 @@
+import type { DatabaseManager } from "$core/manager/database/database.manager";
+import { anyToError } from "$core/utils/error";
+
+export class TextManager {
+
+  private texts: Map<string, string> = new Map();
+
+  constructor(private readonly databaseManager: DatabaseManager) {
+  }
+
+  async load(): Promise<void> {
+    try {
+      const texts = await this.databaseManager.db.text.findMany();
+      this.texts = new Map(texts.map(text => [text.id, text.value]));
+    } catch (error) {
+      this.databaseManager.client.logger.fatal({
+        m: "failed to load texts",
+        e: anyToError(error),
+      });
+    }
+  }
+
+  get(id: string): string {
+    return this.texts.get(id) || "no text found";
+  }
+
+  async reload(): Promise<boolean> {
+    try {
+      const texts = await this.databaseManager.db.text.findMany();
+      this.texts = new Map(texts.map(text => [text.id, text.value]));
+
+      return true;
+    } catch (error) {
+      this.databaseManager.client.logger.error({
+        m: "failed to reload texts",
+        e: anyToError(error),
+      });
+
+      return false;
+    }
+  }
+
+}
