d5978fa2a3da7b5c284383e776ccc1ee70838f44
Arcoz0308 <legrandroi33@gmail.com>
2023-11-26 12:05:19 +0100

    chore(command.util.ts): add getApplicationCommandManager function

--- src/manager/command/command.util.ts
+++ src/manager/command/command.util.ts
@@ -2,6 +2,8 @@ import type { Command } from "$core/base/command";
 import type { Service } from "$core/base/service";
 import type { RESTPostAPIChatInputApplicationCommandsJSONBody } from "discord-api-types/v10";
 import { isDev } from "$core/config/env";
+import type { GuildApplicationCommandManager } from "discord.js";
+import type { Client } from "$core/client";
 
 export const commandMapToAPIArray = (commands: Map<string, Command<Service>>): RESTPostAPIChatInputApplicationCommandsJSONBody[] => {
   if (isDev) {
@@ -9,4 +11,21 @@ export const commandMapToAPIArray = (commands: Map<string, Command<Service>>): R
   }
 
   return [...commands.values()].map(command => command.toJSON());
+};
+
+export const getApplicationCommandManager = (client: Client, guildId: string): GuildApplicationCommandManager => {
+  const manager = client.guilds.cache.get(guildId)?.commands;
+  if (!manager) {
+    client.logger.fatal({
+      m: "failed to find application command manager in guild" + guildId,
+      e: new Error("manager not found"),
+      d: {
+        botId: client.user?.id,
+        guildId: guildId,
+      },
+    });
+    //never run
+    throw new Error("manager not found");
+  }
+  return manager;
 };
