821cde1a96c6e66c4d9b02ea6d6ecd17506528d6
Arcoz0308 <legrandroi33@gmail.com>
2023-11-26 15:47:44 +0100

    feat(main.service.ts): add ServiceLoad interface and handleInteraction method

--- src/service/main/main.service.ts
+++ src/service/main/main.service.ts
@@ -1,9 +1,13 @@
 import { Service } from "$core/base/service/service.class";
 import type { Client } from "$core/client";
+import type { ServiceLoad } from "$core/base/service";
+import { CancelButton } from "$core/base/button/util/cancel_button.class";
+import type { Interaction } from "discord.js";
+import { ApplicationCommandType, ComponentType, InteractionType } from "discord.js";
+import { ID_SEPARATOR } from "$core/base/button/button.const";
 
 // admin commands
-// xp system
-export class MainService extends Service {
+export class MainService extends Service implements ServiceLoad {
 
   name = "main";
 
@@ -12,6 +16,54 @@ export class MainService extends Service {
 
   constructor(client: Client) {
     super(client);
+    this.buttons.set("cancel", new CancelButton(this));
+  }
+
+  load(): void {
+    this.client.on("interactionCreate", (i) => this.handleInteraction(i));
+  }
+
+  handleInteraction(interaction: Interaction): void {
+    switch (interaction.type) {
+      case InteractionType.ApplicationCommand: {
+
+        const command = this.client.commandManager.getCommand(interaction.commandName);
+        if (!command) {
+          this.log.error({
+            m: `command with name ${interaction.commandName} not found`,
+          });
+          return;
+        }
+
+        if (interaction.commandType !== ApplicationCommandType.ChatInput) {
+          this.log.error({
+            m: `command with name ${interaction.commandName} is not a slash command`,
+          });
+          return;
+        }
+
+        void command.preRun(interaction);
+
+        break;
+      }
+      case InteractionType.MessageComponent: {
+        switch (interaction.componentType) {
+          case ComponentType.Button: {
+            const button = this.client.buttonManager.get(interaction.customId.split(ID_SEPARATOR)[0]);
+            if (!button) {
+              this.log.error({
+                m: `button with name ${interaction.customId} not found`,
+              });
+              return;
+            }
+
+            void button.preRun(interaction);
+            break;
+          }
+        }
+        break;
+      }
+    }
   }
 
 }
