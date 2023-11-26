7dbecea8b0cfefaeb67cc4a21a70b623176a9f90
Arcoz0308 <legrandroi33@gmail.com>
2023-11-26 08:06:19 +0100

    feat(string.util.ts): add getMessage function and import TextManager
    chore(string.util.ts): remove unused import
    
    The changes in this commit include:
    
    - Added a new function `getMessage` to the `string.util.ts` file.
    - Imported the `TextManager` class from the `$core/manager/database/text/text.manager` module.
    - Removed an unused import in the `string.util.ts` file.

--- src/utils/function/string/string.util.ts
+++ src/utils/function/string/string.util.ts
@@ -1,4 +1,6 @@
 // code from https://github.com/Virtual-Royaume/Royaume-Discord-Bot/blob/main/$core/utils/message/message.util.ts
+import { TextManager } from "$core/manager/database/text/text.manager";
+
 export const msgParams = (message: string, params: (string | number)[]): string => {
   const words = message.match(/{[^}]+}/g);
 
@@ -7,4 +9,8 @@ export const msgParams = (message: string, params: (string | number)[]): string 
   }
 
   return message;
+};
+
+export const getMessage = (id: string, params: (string | number)[] = []): string => {
+  return msgParams(TextManager.get(id), params);
 };
