955f4d442b468b0584daf375906f52fd7793b585
Arcoz0308 <legrandroi33@gmail.com>
2023-11-26 19:18:55 +0100

    feat(utils/error/error.type.ts): add Result types to Error module

--- src/utils/error/error.type.ts
+++ src/utils/error/error.type.ts
@@ -3,3 +3,7 @@ import type { DebugValues } from "$core/utils/logger/";
 export type DebuggableError = {
   debug: () => DebugValues;
 } & Error
+
+export type ResultOk<T> = [true, T]
+export type ResultError<E extends Error> = [false, E]
+export type Result<T, E extends Error> = ResultOk<T> | ResultError<E>
