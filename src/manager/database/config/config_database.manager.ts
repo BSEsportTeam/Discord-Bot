c17a3d10c7eebfbdc7b3b6557940df32be0042bc
Arcoz0308 <legrandroi33@gmail.com>
2023-11-26 07:48:50 +0100

    feat(config_database.manager.ts): add logging for loaded configs and reloadConfig function

--- src/manager/database/config/config_database.manager.ts
+++ src/manager/database/config/config_database.manager.ts
@@ -11,11 +11,34 @@ export class ConfigDatabaseManager {
   async load(): Promise<void> {
     try {
       this.configs = new Map((await this.databaseManager.db.config.findMany()).map(config => [config.service, config.value]));
+      this.databaseManager.client.logger.info(`loaded with success ${this.configs.size} configs`);
     } catch (error) {
       this.databaseManager.client.logger.fatal({
         m: "failed to load configs",
         e: anyToError(error),
       });
+    }
+  }
+
+  get(name: string): string|null {
+    return this.configs.get(name) || null;
+  }
+
+  async reloadConfig(name: string): Promise<boolean> {
+    try {
+      const config = await this.databaseManager.db.config.findUnique({ where: { service: name } });
+
+      if (!config) return false;
+      this.configs.set(name, config.value);
+
+      this.databaseManager.client.logger.info(`reloaded with success config ${name}`);
+      return true;
+    } catch (error) {
+      this.databaseManager.client.logger.error({
+        m: "failed to reload config",
+        e: anyToError(error),
+      });
+      return false;
     }
   }
 
